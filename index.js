0bc06133b478fcd77491bf62258144fb002601c9
Will <will.rains24@gmail.com>
2021-10-15 10:41:30 +0200

    working to build in functionality to read in from large object of user objects

--- index.js
+++ index.js
@@ -46,6 +46,57 @@ var user2 = {
     ]
 };
 
+var userss = {
+    user1: {
+        userName: '@elonmusk',
+        displayName: 'Elon Musk',
+        joinedDate: 'June 2009',
+        followingCount: 103,
+        followerCount: 47900000,
+        avatarURL: 'assets/elonmusk.jpg',
+        coverPhotoURL: 'assets/elonmusk-cover.jpeg',
+        tweets: [
+            {
+                text: 'I admit to judging books by their cover',
+                timestamp: '2/10/2021 00:01:20'
+            },
+            {
+                text: 'Starship to the moon',
+                timestamp: '2/09/2021 18:37:12'
+            },
+            {
+                text: 'Out on launch pad, engine swap underway',
+                timestamp: '2/09/2021 12:11:51'
+            }
+        ]
+    },
+    user2: {
+        userName: '@BillGates',
+        displayName: 'Bill Gates',
+        joinedDate: 'June 2009',
+        followingCount: 274,
+        followerCount: 53800000,
+        avatarURL: 'assets/billgates.jpg',
+        coverPhotoURL: 'assets/billgates-cover.jpeg',
+        tweets: [
+            {
+                text: 'Everybody asks, how is the next Windows coming along? But nobody asks how is Bill? :/',
+                timestamp: '2/10/2021 00:01:20'
+            },
+            {
+                text: 'Should I start tweeting memes? Let me know in a comment.',
+                timestamp: '2/09/2021 18:37:12'
+            },
+            {
+                text: 'In 2020, I read a book every hour.',
+                timestamp: '2/09/2021 12:11:51'
+            }
+        ]
+    }
+};
+
+
+
 var users = [];
 
 var date1
@@ -141,3 +192,5 @@ function dateDifference(date1) {
 
     return Math.floor((Date.now() - utc1) / _MS_PER_DAY);
 }
+
+//15-Oct - need to make this work off of reading a combined object - object made but cannot get to that
