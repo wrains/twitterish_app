f512cfd6988d20e2850ba3637d11b079f8444155
Will <will.rains24@gmail.com>
2021-10-18 02:34:43 +0200

    added user key value into tweet objects for sorting purposes once time logic is added

--- index2.js
+++ index2.js
@@ -46,66 +46,25 @@ var user2 = {
     ]
 };
 
-//redundant with code below to append objects into one
 
-var userss = {
-    user1: {
-        userName: '@elonmusk',
-        displayName: 'Elon Musk',
-        joinedDate: 'June 2009',
-        followingCount: 103,
-        followerCount: 47900000,
-        avatarURL: 'assets/elonmusk.jpg',
-        coverPhotoURL: 'assets/elonmusk-cover.jpeg',
-        tweets: [
-            {
-                text: 'I admit to judging books by their cover',
-                timestamp: '2/10/2021 00:01:20'
-            },
-            {
-                text: 'Starship to the moon',
-                timestamp: '2/09/2021 18:37:12'
-            },
-            {
-                text: 'Out on launch pad, engine swap underway',
-                timestamp: '2/09/2021 12:11:51'
-            }
-        ],
-    },
-    user2: {
-        userName: '@BillGates',
-        displayName: 'Bill Gates',
-        joinedDate: 'June 2009',
-        followingCount: 274,
-        followerCount: 53800000,
-        avatarURL: 'assets/billgates.jpg',
-        coverPhotoURL: 'assets/billgates-cover.jpeg',
-        tweets: [
-            {
-                text: 'Everybody asks, how is the next Windows coming along? But nobody asks how is Bill? :/',
-                timestamp: '2/10/2021 00:01:20'
-            },
-            {
-                text: 'Should I start tweeting memes? Let me know in a comment.',
-                timestamp: '2/09/2021 18:37:12'
-            },
-            {
-                text: 'In 2020, I read a book every hour.',
-                timestamp: '2/09/2021 12:11:51'
-            }
-        ],
-    }
-};
-console.log(userss);
+//make this to be more dynamic with nested for loops
+for (let tweet of user1.tweets) {
+    tweet["user"] = 'user1';
+}
+for (let tweet of user2.tweets) {
+    tweet["user"] = 'user2';
+}
+
+//add time since tweet to tweet using above for loops
 
 userss = { user1, user2 };
 
 console.log(userss);
 
-
 var container = document.getElementById('container');
 
 var countOfTweets = 0;
+
 
 
 var allTweets = [];
